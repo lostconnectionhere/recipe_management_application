d4e1c7d0b664c7e428f164273c1f8e45ae3ec2d3
Roz <rozsabir@Rozs-MacBook-Pro.local>
2022-12-02 13:02:55 +0100

    create db again but with the correct values

--- database_exercise.py
+++ database_exercise.py
@@ -1,6 +1,9 @@
 import sqlite3
 
 connection = sqlite3.connect('recipes.db')
+cursor = connection.cursor()
+
+cursor.execute("CREATE TABLE Recipes (recipe_name text, cooking_time integer, country_of_origin text")
 
 recipe_list =[
     ('Bahn Mi', 25, 'Vietnam'),
@@ -10,5 +13,7 @@ recipe_list =[
     ('Chicken Satay', 30, 'Indonesian'),
     ('Korean Oven Chicken Wings', 60, 'Korea')    
 ]
+
+cursor.executemany("insert into Recipes values (?,?,?)", recipe_list)
 
 connection.close()
