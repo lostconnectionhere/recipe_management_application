b636ccb0398e2e577cbebba739e6212731fb4a29
Roz <rozsabir@Rozs-MacBook-Pro.local>
2022-12-02 13:58:24 +0100

    added new table diet to the db

--- database_exercise.py
+++ database_exercise.py
@@ -3,7 +3,7 @@ import sqlite3
 connection = sqlite3.connect('recipes.db')
 cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE Recipes (recipe_name text, cooking_time integer, country_of_origin text")
+cursor.execute("CREATE TABLE Recipes (recipe_name text, cooking_time integer, country_of_origin text)")
 
 recipe_list =[
     ('Bahn Mi', 25, 'Vietnam'),
@@ -14,6 +14,21 @@ recipe_list =[
     ('Korean Oven Chicken Wings', 60, 'Korea')    
 ]
 
-cursor.executemany("insert into Recipes values (?,?,?)", recipe_list)
+cursor.executemany("INSERT INTO Recipes VALUES (?,?,?)", recipe_list)
 
+#print rows of Recipes table 
+for row in cursor.execute("SELECT * FROM Recipes"):
+    print(row)
+
+#print specific rows
+# cursor.execute("SELECT * FROM Recipes WHERE country_of_origin=:x", {'x': 'Korea'})
+# recipe_search = cursor.fetchall
+# print(recipe_search)
+
+cursor.execute("CREATE TABLE Diet (recipe_name text, type_of_diet text, allergies text)")
+cursor.execute("INSERT INTO Diet VALUES (?,?,?)", ('Bahn Mi', 'Non-vegetarian', 'peanuts shellfish gluten'))
+print("--------------------------------------------")
+#print rows of Diet table 
+for row in cursor.execute("SELECT * FROM Diet"):
+    print(row)
 connection.close()
