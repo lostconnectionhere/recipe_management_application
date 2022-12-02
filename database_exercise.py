ef9daf838bd3bd471fee158164a2396935004008
Roz <rozsabir@Rozs-MacBook-Pro.local>
2022-12-02 12:19:58 +0100

    example db with sqlite

---
+++ database_exercise.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+sqlite3.connect('recipes')
+
+recipe_list =[
+    ('Bahn Mi', 25, 'Vietnam'),
+    ('Mushroom Risotto', 30, 'Italy'),
+    ('Brown Beans with Chicken', 30, 'Surinam'),
+    ('Danish Frikadell', 20, 'Denmark'),
+    ('Chicken Satay', 30, 'Indonesian'),
+    ('Korean Oven Chicken Wings', 60, 'Korea')    
+]
